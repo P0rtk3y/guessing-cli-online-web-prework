817d254b530cf43a8c2c68bffa648e5d7de2fd58
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-17 01:28:43 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -10,7 +10,7 @@ def run_guessing_game
     puts "Guess a number between 1 and 6."
     input = gets.chomp
     number = number_generator
-      if input.downcase != 'exit'
+      if input.downcase == 'exit'
         puts "Goodbye!"
         break 
       elsif input.to_i == number 
